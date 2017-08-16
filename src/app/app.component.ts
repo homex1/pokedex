412caf7769c888b40ab0780533624acd28895048
Carlos <homex_1@hotmail.com>
2017-08-16 11:46:53 -0500

    Remove pokemon details

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,7 +12,6 @@ import { PokemonService } from './pokemon.service';
 export class AppComponent implements OnInit {
   title = 'Pokedex';
   pokemones: Pokemon[];
-  pokemon: Pokemon;
 
   constructor(private pokemonService: PokemonService) { }
 
@@ -22,9 +21,5 @@ export class AppComponent implements OnInit {
 
   getPokemones(): void {
     this.pokemonService.getPokemones().then(pokemones => this.pokemones = pokemones);
-  }
-
-  onSelect(pokemon: Pokemon, index: number): void {
-    this.pokemon  = pokemon;
   }
 }
